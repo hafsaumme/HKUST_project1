b66d89102b54a902cdee01b81ce49900f9157dfb
hafsaumme <89129027+hafsaumme@users.noreply.github.com>
2021-08-18 23:45:43 +0800

    Delete job directory

--- job/admin/includes/footer.inc.php
+++
@@ -1,2 +0,0 @@
-<p>Copyright (c) 2016 jobscope. All rights reserved. <br />
-			Designed by UMME HAFSA</p>
--- job/includes/logo.inc.php
+++
@@ -1,7 +0,0 @@
-
-	
-
-	<h1>jobscope</h1>
-<img src="logo.png" alt="">
-	<p>jobs for you</p>
-	
